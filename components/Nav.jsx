c930941ed944fdb19ca3d02f60ebde1336b9b4b0
rafaasousa <rafael.p.sousa@outlook.com>
2024-10-21 11:13:23 -0300

    create stair transition

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -6,25 +6,45 @@ import { usePathname } from "next/navigation";
 const links = [
   {
     name: "home",
-    path: "/"
+    path: "/",
   },
   {
     name: "services",
-    path: "/services"
-  },{
-    name: "resume",
-    path: "/resume"
-  },{
-    name: "work",
-    path: "/work"
-  },{
-    name: "contact",
-    path: "/contact"
+    path: "/services",
   },
-]
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
 
 const Nav = () => {
-  return <nav>Desktop Nav</nav>;
+  const pathname = usePathname();
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === pathname && "text-accent border-b-2 border-accent"
+            } capitalize font-medium hover:text-accent transition-all`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
