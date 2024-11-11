7aa58ce9af28e0e1017055b7dcabf21866b25a2a
rafaasousa <rafael.p.sousa@outlook.com>
2024-11-11 20:26:25 -0300

    contact page update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,36 +1,39 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
+import Link from "next/link";
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const info = [
   {
     icon: <FaPhoneAlt />,
     title: "Phone",
     description: "(+55 11) 96070 2307",
+    live: "https://wa.link/9y2qro",
   },
   {
     icon: <FaEnvelope />,
     title: "Email",
     description: "rafael.p.sousa@hotmail.com",
+    live: "mailto: rafael.p.sousa@hotmail.com",
+  },
+  {
+    icon: <FaGithub />,
+    title: "GitHub",
+    description: "RafaaSousa",
+    live: "https://www.github.com/RafaaSousa",
+  },
+  {
+    icon: <FaLinkedinIn />,
+    title: "LinkedIn",
+    description: "Rafael Sousa",
+    live: "https://www.linkedin.com/in/rafael-p-sousa",
   },
   {
     icon: <FaMapMarkerAlt />,
     title: "Address",
-    description: "Rua Rio preto, 150 - Atibaia",
+    description: "Atibaia - SP",
+    live: "https://www.google.com/maps/place/Atibaia,+SP/@-23.1228775,-46.6412251,12z/data=!3m1!4b1!4m6!3m5!1s0x94cec11f46f1007d:0xc885a2d1bf078e37!8m2!3d-23.1155873!4d-46.5532067!16zL20vMDJ2X2N4?entry=ttu&g_ep=EgoyMDI0MTEwNi4wIKXMDSoASAFQAw%3D%3D",
   },
 ];
 
@@ -47,59 +50,23 @@ const Contact = () => {
       className="py-6"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px]">
-          {/* form */}
-          <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
-              <p className="text-white/60">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                pariatur quidem eveniet neque laboriosam!
-              </p>
-              {/* input */}
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname" />
-                <Input type="lastname" placeholder="Lastname" />
-                <Input type="email" placeholder="Email" />
-                <Input type="Phone" placeholder="Phone number" />
-              </div>
-              {/* select */}
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a service" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value="est">Web Development</SelectItem>
-                    <SelectItem value="cst">UI/UX Design</SelectItem>
-                    <SelectItem value="mst">Logo Design</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              {/* textarea */}
-              <Textarea
-                className="h-[200px]"
-                placeholder="Type your message here."
-              />
-              {/* btn */}
-              <Button size="md" className="max-w-40">
-                Send message
-              </Button>
-            </form>
-          </div>
+          
           {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul className="flex flex-col gap-10">
+          <div className="flex items-center order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col xl:grid xl:grid-cols-3 gap-10">
               {info.map((item, index) => {
                 return (
-                  <li key={index} className="flex items-center gap-6">
-                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                      <div className="text-[28px]">{item.icon}</div>
-                    </div>
-                    <div className="flex-1">
-                      <p className="text-white/60">{item.title}</p>
-                      <h3 className="text-xl">{item.description}</h3>
+                  <li key={index} className="flex items-center gap-10">
+                    <div className="flex">
+                    <Link href={item.live} target="_blank" className="flex items-center gap-3">
+                      <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                        <div className="text-[28px]">{item.icon}</div>
+                      </div>
+                      <div className="flex flex-col">
+                        <p className="text-white/60">{item.title}</p>
+                        <h3 className="text-xl">{item.description}</h3>
+                      </div>
+                    </Link>
                     </div>
                   </li>
                 );
@@ -107,7 +74,6 @@ const Contact = () => {
             </ul>
           </div>
         </div>
-      </div>
     </motion.section>
   );
 };
