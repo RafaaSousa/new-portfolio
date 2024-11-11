bd24d1d603697b668c795ce74701f9ecc358df0b
rafaasousa <rafael.p.sousa@outlook.com>
2024-11-11 20:29:38 -0300

    create Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-4 text-center">
+      <div className="border border-white/10 my-5"></div>
+      <p className="text-white/30">© 2024 Rafael Sousa. Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
